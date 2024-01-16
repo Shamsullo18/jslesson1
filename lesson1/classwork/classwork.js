4976d85b8dd493f1886907693e3565bc8d7992eb
Shamsullo18 <sjomahmadov@gmail.com>
2024-01-16 16:11:02 +0500

    lesson1

---
+++ lesson1/classwork/classwork.js
@@ -0,0 +1,18 @@
+let num1 =1;
+let num2 =2;
+let suma = num1 = num2;
+console.log("sum", summa);
+function NolPosNeg (k){
+ if (k > 0){
+    console.log("positive")
+ }
+ if (k < 0){
+    console.log("negative")
+ }
+ else{
+    console.log("zero")
+ }
+}
+let n = 1;
+for (let i=1; i<=10; ++i)
+console.log(n + "*" + i + "=" + n * i);
